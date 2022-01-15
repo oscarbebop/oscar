e72afe7231e32e751b8d04d5824dd7399a3659cb
Oscar <hello@castaneda.io>
2022-01-14 20:08:31 -0600

    graphcms and codegen integrated

---
+++ codegen.js
@@ -0,0 +1,18 @@
+const { loadEnvConfig } = require("@next/env");
+loadEnvConfig(process.cwd());
+
+module.exports = {
+  overwrite: true,
+  schema: process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT,
+  documents: null,
+  generates: {
+    "generated/graphql.ts": {
+      plugins: [
+        "typescript",
+        "typescript-operations",
+        "typescript-react-apollo",
+      ],
+    },
+    "./graphql.schema.json": { plugins: ["introspection"] },
+  },
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,36 @@
 import type { AppProps } from "next/app";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const authLink = setContext((_, { headers }) => {
+    return {
+      headers: {
+        ...headers,
+        authorization: `Bearer ${process.env.NEXT_PUBLIC_TOKEN}`,
+      },
+    };
+  });
+
+  const httpLink = createHttpLink({
+    uri: process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT,
+  });
+
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
 export default MyApp;
